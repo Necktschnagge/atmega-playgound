96594479f851fc93051e6ce8fb365ff16405d147
Maximilian Starke <maximilian.starke@medionmail.com>
2017-05-27 09:33:54 +0200

    half-ready scheduler

--- src/Fussl-01/Fussl-01/scheduler.cpp
+++ src/Fussl-01/Fussl-01/scheduler.cpp
@@ -4,62 +4,154 @@
  * Created: 07.03.2017 18:15:40
  *  Author: Maximilian Starke
  
+ FPS:	see at .h file
  
  */
 
-#include "scheduler.h"
 #include <avr/interrupt.h>
+
+#include "scheduler.h"
 
 using namespace time;
 
-ISR (TIMER1_COMPA_vect){
-	/* compare match interrupt routine */
-	++scheduler::divisions_of_second;
-	//### somewhere in update() we need to check  the overflow;
-	if (scheduler::divisions_of_second > 0xF000){
-		/* scheduler is not fast enough */
-		scheduler::updateNowTime();
-		//### check that we don't have missed some interrupts yet.
-	}
+namespace scheduler {
+	SystemTime SystemTime::instance{}; // default constructor
 }
 
-uint16_t scheduler::divisions_of_second {0};
-HumanTime scheduler::now;
-uint16_t scheduler::nextFreeHandle {1};
-void* scheduler::taskTable {nullptr};
-uint16_t scheduler::taskTableSize {0};
+ISR (TIMER1_COMPA_vect){
+	// set the compare-match value for the next interrupt
+	// update the SystemTime::instance
+	
+	OCR1A = scheduler::SystemTime::__timer__counter__compare__value__only__called__by__interrupt__();
+	// this value must not over or underflow. check this!!!
+	
+	// a problem would be if the calculation of the new compare match is not ready until the next osc rising edge
+	// ##detect this.
+	
+	++scheduler::SystemTime::instance;
+}
+
+scheduler::SystemTime::SystemTime() : now(0), ref_time(0) {
+	init(DEFAULT_PRECISION,DEFAULT_OSC_FREQUENCY,0.0L);
+}
+
+bool scheduler::SystemTime::is_precision_possible(uint8_t precision){
+	// can be interrupted, no problem
+	if (precision > 16) return false; // accuracy exceed of SystemTime itself
+	constexpr uint32_t int1_32 {1u};
+	if ((int1_32 << precision) > osc_frequency ) return false;
+		// SystemTime cannot be more accurate than the oscillator
+		// hier fehlt noch der puffer bei annähernd ==, wenn das padding anfängt negativ zu werden
+		
+	if ( (osc_frequency / (int1_32 << precision)) >= (int1_32 << 15) ) return false;
+		// TCNT can only count up to 2^16-1 /// puffer ist hier auch eingebaut nach oben
+	return true;
+}
+
+bool scheduler::SystemTime::change_precision_aborting(uint8_t precision){
+	// can be interrupted, no problem
+	if (is_precision_possible(precision)){
+		this->precision = precision;
+		return true;
+	}
+	return false;
+}
+
+uint8_t scheduler::SystemTime::change_precision_anyway(uint8_t precision){
+	// can be interrupted, no problem
+	while (precision > 0){
+		if (change_precision_aborting(precision)) return precision;
+		--precision;
+	}
+	while (precision < 64){// far too big but possible
+		if (change_precision_aborting(precision)) return precision;
+		++precision;
+	}
+	return PRECISION_ERROR;
+}
+
+const scheduler::SystemTime& scheduler::SystemTime::operator ++ (){
+	uint8_t sreg = SREG;
+		// do not interrupt
+		// of course: if called by the ISR interrupts are already deactivated,
+		// but in case someone else calls this function despite I cannot imagine any purpose to do that
+	cli();
+	now = now + ( 1ull << (16 - old_precision) );
+	old_precision = precision;
+	SREG = sreg;
+	return *this;
+}
+
+bool scheduler::SystemTime::init(uint8_t precision, uint32_t osc_frequency, const long double& clock_precision){
+	stop();
+	// because you should not use this function to change while SystemTime is running
+	// shoukld be called by the constructor ## check this
+	
+	instance.refinement = clock_precision;
+	instance.osc_frequency = osc_frequency;
+	instance.precision = 12; // for the error case // why <<<<
+	uint8_t result = instance.change_precision_anyway(precision);
+	instance.old_precision = instance.precision;
+	
+	return (result != PRECISION_ERROR);
+}
+
+bool scheduler::SystemTime::start(){
+	if (instance.is_precision_possible(instance.precision) == false) return false;
+	
+	// <<<<< if already started ... (do not handle) ??
+	
+	cli(); // deactivate global interrupts
+	
+	TCCR1A = 0x00; // ~ NO PWM
+	TCNT1 = 0; // counter starts at zero
+	
+	TIMSK = (TIMSK & 0b11000011) | 0b00010000; // data sheet page 138: interrupt enable for Timer1 Compare Match A
+	
+	OCR1A = __timer__counter__compare__value__only__called__by__interrupt__(); // register with compare value
+	
+	TCCR1B = 0b00001111; // CTC (Clear Timer on Compare Match) and start timer running from ext source on,
+						 //triggered rising edge
+						 
+	sei();	// activate global interrupts
+	return true;
+}
+
+void scheduler::SystemTime::stop(){
+	TIMSK &= 0b11000011;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  void scheduler::init(void* taskTableSpaceJunk, uint16_t taskTableSize){
 	 /* using 16bit Timer1 */
 	 
 	 scheduler::taskTable = taskTableSpaceJunk;
 	 scheduler::taskTableSize = taskTableSize;
-	 
-	 //init time
-	 
-	 /*now.day = 1;
-	 now.year = 2017;
-	 now.hour = 0;
-	 now.minute = 0;
-	 now.second = 0;
-	 *///###must work around
-	 
-	 cli(); // deactivate global interrupts
-	 
-	 TCCR1A = 0x00; // ~ NO PWM
-	 TCNT1 = 0; // counter starts at zero
-	 TIMSK |= 0b00010000; // data sheet page 138: interrupt enable for Timer1 Compare Match A
-	 // activate global interrupts !!!
-	 OCR1A = /*1024*/ 1 << (15 - PARTS_OF_SECOND_LOG); // 1 second divided into 32 equal parts.
-	 static_assert(1 << (15 - PARTS_OF_SECOND_LOG) == 1024,"bla bla");
-	 // register with compare value
-	 
-	 TCCR1B = 0b00001111;	// CTC (Clear Timer on Compare Match) and start timer running from ext source on,
-	 //triggered rising edge
-	 // 32768Hz external oscillator for timing clock
-	 sei();	// activate global interrupts
- }
+}
 
+/*
 uint16_t scheduler::updateNowTime(){
 	uint16_t forwardSeconds;
 	uint16_t partOfSecond;
@@ -71,10 +163,16 @@ uint16_t scheduler::updateNowTime(){
 	SREG = sreg;
 	while (forwardSeconds){
 		--forwardSeconds;
-		++now;
+		//++now;
 	}
 	return partOfSecond;
 }
+*/
+
+uint16_t scheduler::nextFreeHandle {1};
+void* scheduler::taskTable {nullptr};
+uint16_t scheduler::taskTableSize {0};
+
 
 void scheduler::run(){
 	//uint16_t partOfSecond = updateNowTime();
@@ -83,3 +181,82 @@ void scheduler::run(){
 	// look for tasks to be executed
 	
 }
+
+
+
+
+
+
+#if false
+// using timer
+// 16 bit timer/counter to count the RTC Quartz signal which is 32768 Hz
+//we need to setup the 16 bit timer
+TCCR1A = 0x00; // we don't use PWM (for creating a specific output signal) and we don't set any output pins
+TCCR1B = 0b00001000; // timer turned off; // 1 stands for CTC (Clear Timer on Compare Match) with OCR1A
+// CTC1 must be 1 to set the counter 0 when it reaches the compare value, we have no prescaler because of external source
+TCCR1B = 0b00001111; // count on rising edge, timer runs.
+// TCCR1C is per default 0 so we can leave this out.
+
+// 32768 external oscillator for timing clock
+// 16MHz CPU Clock
+
+OCR1A = 1024; // 1 second divided into 32 equal parts
+//OCR1AH = 0x08; // high comparison byte // only this order first high than low, please deactivate global interrupts before changing
+//OCR1AL = 0x00; // low c byte			8*256 = 2048 // 8 interrupts per second
+//OCR1A = 0x0FFF; // directly
+
+// don't know why there are an a, b , c register
+
+// please deactivate interrupts
+TCNT1H =  0; // counter starts a zero
+TCNT1L = 0; //
+TCNT1 = 0;
+
+TIMSK |= 0b00010000; // data sheet page 138: interrupt enable for Timer1 Compare Match A
+// activate global interrupts !!!
+
+#endif // static comment
+
+
+
+
+/*
+namespace month_string {
+	namespace english {
+		const char JAN[] = "January";
+		const char FEB[] = "February";
+		const char MAR[] = "March";
+		const char APR[] = "April";
+		const char MAY[] = "May";
+		const char JUN[] = "June";
+		const char JUL[] = "July";
+		const char AUG[] = "August";
+		const char SEP[] = "September";
+		const char OCT[] = "October";
+		const char NOV[] = "November";
+		const char DEC[] = "December";
+		
+		const char * const months[] {JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC};
+	};
+	
+	namespace german {
+		const char JAN[] = "Januar";
+		const char FEB[] = "Februar";
+		const char MAR[] = "Maerz";
+		const char* const APR = english::APR;
+		const char MAY[] = "Mai";
+		const char JUN[] = "Juni";
+		const char* const JUL = english::JUL;
+		const char* const AUG = english::AUG;
+		const char* const SEP = english::SEP;
+		const char OCT[] = "Oktober";
+		const char* const NOV = english::NOV;
+		const char DEC[] = "Dezember";
+		
+		const char* const months[] {JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC};
+	};
+}*/
+
+
+
+
--- src/Fussl-01/Fussl-01/scheduler.h
+++ src/Fussl-01/Fussl-01/scheduler.h
@@ -1,18 +1,14 @@
 /*
  * scheduler.h
  *
- * Created: 07.03.2017 18:16:16
+ *  Created: 07.03.2017 18:16:16
  *  Author: Maximilian Starke
- 
  *
  *
  *
  *
  *
  FPS:		everything needs to be completed of course....
-			class Time seems to be ready, check conversions between .. and ExtendedTime
-			make up ExtendedMetricTime
-			typedef new shorter abbrevs,... EMT(ime), ETime, Time
 			
 			... make a short prototype of the scheduler to follow our issues.
  */ 
@@ -20,91 +16,177 @@
 
 #ifndef SCHEDULER_H_
 #define SCHEDULER_H_
-//#include <time.h>
-#include <stdint.h> // necessary???<<<
+//#include <avr/interrupt.h>
+
+#include <stdint.h> // by the way... it is already included by f_time.h
+#include <math.h>
 #include "f_time.h"
 
-#ifdef NNNNNNNNN
-// using timer
-// 16 bit timer/counter to count the RTC Quartz signal which is 32768 Hz
-//we need to setup the 16 bit timer
-TCCR1A = 0x00; // we don't use PWM (for creating a specific output signal) and we don't set any output pins
-TCCR1B = 0b00001000; // timer turned off; // 1 stands for CTC (Clear Timer on Compare Match) with OCR1A
-// CTC1 must be 1 to set the counter 0 when it reaches the compare value, we have no prescaler because of external source
-TCCR1B = 0b00001111; // count on rising edge, timer runs.
-// TCCR1C is per default 0 so we can leave this out.
-
-// 32768 external oscillator for timing clock
-// 16MHz CPU Clock 
-
-OCR1A = 1024; // 1 second divided into 32 equal parts
-//OCR1AH = 0x08; // high comparison byte // only this order first high than low, please deactivate global interrupts before changing
-//OCR1AL = 0x00; // low c byte			8*256 = 2048 // 8 interrupts per second
-//OCR1A = 0x0FFF; // directly
-
-// don't know why there are an a, b , c register
-
-// please deactivate interrupts
-TCNT1H =  0; // counter starts a zero
-TCNT1L = 0; //
-TCNT1 = 0;
-
-TIMSK |= 0b00010000; // data sheet page 138: interrupt enable for Timer1 Compare Match A
-// activate global interrupts !!!
-
-#endif // static comment
-
-
-/*
-namespace month_string {
-	namespace english {
-		const char JAN[] = "January";
-		const char FEB[] = "February";
-		const char MAR[] = "March";
-		const char APR[] = "April";
-		const char MAY[] = "May";
-		const char JUN[] = "June";
-		const char JUL[] = "July";
-		const char AUG[] = "August";
-		const char SEP[] = "September";
-		const char OCT[] = "October";
-		const char NOV[] = "November";
-		const char DEC[] = "December";
-		
-		const char * const months[] {JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC};
-	};
-	
-	namespace german {
-		const char JAN[] = "Januar";
-		const char FEB[] = "Februar";
-		const char MAR[] = "Maerz";
-		const char* const APR = english::APR;
-		const char MAY[] = "Mai";
-		const char JUN[] = "Juni";
-		const char* const JUL = english::JUL;
-		const char* const AUG = english::AUG;
-		const char* const SEP = english::SEP;
-		const char OCT[] = "Oktober";
-		const char* const NOV = english::NOV;
-		const char DEC[] = "Dezember";
-		
-		const char* const months[] {JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC};
-	};
-}*/
-
-
-
-
-/* scheduler -----------------------------------------------------------------------------------------------------  */
+/* scheduler ----------------------------------------------------------------------------------------------------  */
 
 namespace scheduler {
 	
-	typedef uint16_t SCHEDULE_HANDLE;
-	constexpr uint16_t NO_HANDLE {0};
-	
-	constexpr uint16_t PARTS_OF_SECOND_LOG {5}; // 32 parts of second
-	constexpr uint16_t PARTS_OF_SECOND {1 << PARTS_OF_SECOND_LOG};
+		/* Singleton */
+	class SystemTime {
 		
+			// 32768Hz external oscillator for timing clock
+		static constexpr uint32_t DEFAULT_OSC_FREQUENCY { 1u << 15 };
+		static constexpr uint8_t DEFAULT_PRECISION {10u};
+		static constexpr uint8_t PRECISION_ERROR {0xFF};
+		
+			/* precision is the logarithmic expression how accurate the time resolution of SystemTime is
+				precision says the second is divided into 2^precision equal parts
+				so with precision=0 the smallest resolution is a second,
+				with precision=10 the resolution will be 1/1024 s
+				constraints: 0<= precision <= ld(OSC_FREQUENCY) // natural constraints
+				OSC_FREQUENCY / 2^(precision)  < 2^16 // cannot be = 2^16. This is the TCNT OV.
+					// We always want to make compare match for better code density and less complexity
+				*/
+		uint8_t precision;
+			/* purpose: changing precision during timer run:
+				current timer gap must be calculated with old_precision,
+				next must be initiated by precision*/
+		uint8_t old_precision;
+		
+			/* PHASE: a phase is the time gap / time area between two compare matchings */
+		
+			/* the frequency of the RTC oscillator */
+		uint32_t osc_frequency;
+	
+			/* amount of additional ticks to wait for during one phase,
+				given by user or admin 
+				it does not contain the ticks which were kicked out be the integer division
+				in order to calculate the Compare-Match-Value.
+				That (necessary) fix is already treated by get_time_padding() */
+		long double refinement;
+			// ä## must be changed i.e. adapted when precision is changed!!!
+		
+			/* when the controller starts (booting process) 'now' should be initialized to zero
+			   then it values the distance from now to the begin of computing time */
+		time::ExtendedMetricTime now;
+			/* last time of setting RTC as refinement reference */
+		time::ExtendedMetricTime ref_time;
+		
+		SystemTime();
+					 //## precision gültig machen
+		
+			/* return the (maybe negative) padding to add to the normal TNCTCompareValue
+				*/
+		inline int16_t get_time_padding(){
+			static long double padding {0};
+			padding += refinement
+					+ static_cast<long double>(osc_frequency % (1<<precision))
+							/ static_cast<long double>(1<<precision);
+			// extract the int part of padding
+			long double trunced = trunc(padding);
+			padding -= trunced;
+			return static_cast<int16_t>(trunced); // ### think about test cases how are negative numbers trunced?
+		}
+		
+		inline uint16_t get_normal_TCNT_compare_value(){ // Timer Counter 
+			return osc_frequency / (1 << precision); // damit das valid ist, müssen wir immer die gültigkeit von precision prüfen
+		}
+		
+		bool is_precision_possible(uint8_t precision);
+			
+	public:
+	
+		SystemTime(const SystemTime&) = delete;
+		const SystemTime& operator = (const SystemTime&) = delete;
+		SystemTime(SystemTime&&) = delete;
+		const SystemTime& operator = (SystemTime&&) = delete;
+		
+		static inline uint16_t __timer__counter__compare__value__only__called__by__interrupt__() {
+			//### check overflows / underflows
+			int32_t value;
+			
+		again:
+			value = static_cast<int32_t>(instance.get_normal_TCNT_compare_value()) + static_cast<int32_t>(instance.get_time_padding());
+			
+		begain:
+			if (value == 0){
+				// well, I think this is okay. It happens when we use maximum precision and or osc is a slower than it should be.
+				// but this case should really not appear far more than one time again during one function call.
+				++instance;
+				goto again;
+			}
+			if (value < 0){
+				// ### log any kind of error. this case should never happen!!!!
+				// we should stop immediately.
+				value = 0;
+				goto begain;
+				// osc frequency is far from its value
+			}
+			if (value > 0xFFFE){ // FFFF must also be possible
+				// osc_frequency far from its value
+				// ## log error
+				value = 0xFFFF;
+			}
+			
+			return static_cast<uint16_t>(value);
+		}
+
+		static SystemTime instance;
+		
+		
+		// such a changing process must wait till the current second is over or not ???###
+		/// -> no it neednt but the last phase must be treated with the old precision
+		/// ### think about which procedures must deactivate interrupts during their execution	
+
+			/* try to change the precision (and return true).
+			   if not possible than don't change anything and return false */
+		bool change_precision_aborting(uint8_t precision);
+		
+			/* try to change the precision to the given value,
+			   if not possible the function tries to find a possible precision in the near of given precision
+			   returns this->precision after change or 0xFF as an error code for "no possible precision" */
+		uint8_t change_precision_anyway(uint8_t precision);
+
+			/* increase the instance's now time by the time delta given indirect by precision */
+			/* more exactly: given by old_precision and ser the old_precision to precision */
+		const SystemTime& operator ++ ();
+		
+			/* static version of operator ++ on singleton instance */
+		inline static void tick_forward(){
+			instance.operator ++();
+		}
+		
+			/* init the SystemTime singleton */
+		static bool init(uint8_t precision = DEFAULT_PRECISION, uint32_t osc_frequency = DEFAULT_OSC_FREQUENCY, const long double& clock_precision = 0.0L);
+			//##not impl
+		
+			/* start running SystemTime clock */
+		static bool start();
+			// ## not impl both
+			
+			/* stop running SystemTime clock */
+		static void stop();
+		
+	};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	typedef uint16_t SCHEDULE_HANDLE; /// no make a wrapper, a class with one uint16_t
+	
+	constexpr SCHEDULE_HANDLE NO_HANDLE {0};
+	
+	//constexpr uint16_t PARTS_OF_SECOND_LOG {5}; // 32 parts of second
+	
+	//constexpr uint16_t PARTS_OF_SECOND {1 << PARTS_OF_SECOND_LOG};
+	
 	class Priority{
 		uint8_t state;
 		
@@ -115,7 +197,7 @@ namespace scheduler {
 	};
 	
 	extern uint16_t divisions_of_second; // 0 ... PARTS_OF_SECOND - 1
-	extern time::HumanTime now;
+//	extern time::HumanTime now;
 	extern uint16_t nextFreeHandle;
 	extern void* taskTable;
 	extern uint16_t taskTableSize;
@@ -138,11 +220,6 @@ namespace scheduler {
 		uint8_t flags; //{ valid, Timer/Task, Callable/procedure, }
 		
 	};
-	
-	class ExactTime {
-		time::HumanTime time;
-		uint8_t divisions_of_second;
-		};
 	
 	class UCallback {
 		uint16_t dummy;
@@ -170,10 +247,10 @@ namespace scheduler {
 		uint8_t flags;
 		};
 	class TimerMemLineTime {
-		ExactTime time;
+		//ExactTime time;
 		};
 	class TimerMemLineTime2 {
-		ExactTime time; // but we can leave out the year.
+		//ExactTime time; // but we can leave out the year.
 		uint16_t repeatings;
 	};
 	
