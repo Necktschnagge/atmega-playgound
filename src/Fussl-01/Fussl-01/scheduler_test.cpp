78140c14fa2497383199c12dda822fc4c330f671
Maximilian Starke <maximilian.starke@medionmail.com>
2018-11-21 19:15:00 +0100

    test stuff

--- src/Fussl-01/Fussl-01/scheduler_test.cpp
+++ src/Fussl-01/Fussl-01/scheduler_test.cpp
@@ -9,11 +9,11 @@
 
 void test_system_time(){
 	
-	uint8_t log_precision{ 12 };
+	//uint8_t log_precision{ 12 };
 	
 	uint8_t error_code{ 0 };
 	
-	fsl::os::system_time my_system_time(1L<<15, log_precision, error_code);
+	//fsl::os::system_time my_system_time(1L<<15, log_precision, error_code);
 	
 	if (error_code){
 		led::clear();
@@ -21,23 +21,24 @@ void test_system_time(){
 		hardware::delay(5000);
 	}
 	
-	fsl::os::system_time::link(my_system_time);
+	//fsl::os::system_time::link(my_system_time);
 	
-	bool start_code = fsl::os::system_time::start();
+	//bool start_code = fsl::os::system_time::start();
 	
-	if (! start_code){
-		led::clear();
-		led::printString("ERR-time");
-		hardware::delay(5000);
-	}
-	TCCR1B &= ~0b00000111;
-	TCCR1B |= 0b00000101;
+	TCNT1 = 0;
+	//TCCR1B &= ~0b00000111;
+	TCCR1B = 0b00000111;
 	while(true){
 		//time::EMT before = my_system_time();
 		//led::printInt(before.get_in_seconds());
 		led::printInt(TCNT1);
 		hardware::delay(500);
 		led::clear();	
+		led::printInt(TCNT1);
+		led::printDotsOnly(0xFF);
+		hardware::delay(500);
+		led::clear();
+		
 	}
 	
 }
