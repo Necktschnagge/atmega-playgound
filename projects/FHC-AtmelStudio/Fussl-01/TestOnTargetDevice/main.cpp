09a8a08a95c9fe2621acd9817ff213437a4a9e11
Maximilian Starke <maximilian.starke@medionmail.com>
2019-09-21 12:30:32 +0200

    remove some demo sample code for static_task_runner

--- projects/FHC-AtmelStudio/Fussl-01/TestOnTargetDevice/main.cpp
+++ projects/FHC-AtmelStudio/Fussl-01/TestOnTargetDevice/main.cpp
@@ -11,45 +11,14 @@
 #include "test_f_range_int.h"
 #include "f_test_logger.h"
 
-#include "f_static_task_runner.h"
-
-void f1(){
-	bool a;
-}
-void f2(){
-	bool a;
-}
-void g1(){
-	bool a;
-}
-void g2(){
-	bool a;
-}
-
-void h1(){
-	bool a;
-}
-
-void h2(){
-	bool a;
-}
-
-using c1 = static_task_runner<void(*)(),f1,f2>;
-using c2 = static_task_runner<void(*)(),g1,g2>;
-using c3 = callback_traits<void(*)()>::static_task_runner<h1,h2>;
-
-using t = concat<c1,c3,c2>;
-
 
 int main(void)
 {
-	
-	t::run();
-	
 	f_test_logger test_logger;
 	
 	run_test_f_range_int(test_logger);
 
+//test::range_int::runner::run(test_logger);
 
 
 	while (true){
@@ -60,9 +29,8 @@ int main(void)
 		// "output test results"
 		if (test_logger.any_fail()){
 			for (auto it = test_logger.errors().cbegin(); it != test_logger.errors().cend(); ++it){
-				const char* error_message{ it->cbegin() }; // pint to first character of single error message.
-				error_message++;
-				DDRB = 0;
+				const char* const error_message{ it->cbegin() }; // pint to first character of single error message.
+				const char* cc{ error_message + 1 };
 			}
 		}
 	}
