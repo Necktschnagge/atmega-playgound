61e7627d014b76183a4831e42b70b247a7e62596
Maximilian Starke <maximilian.starke@medionmail.com>
2019-09-29 21:28:11 +0200

    minor update:  unit test

--- projects/FHC-AtmelStudio/Fussl-01/TestOnTargetDevice/test_f_range_int.cpp
+++ projects/FHC-AtmelStudio/Fussl-01/TestOnTargetDevice/test_f_range_int.cpp
@@ -56,21 +56,23 @@ namespace test {
 		} // namespace classes
 		namespace types {
 			template<class _BASE_TYPE>
-			inline void base_type(f_test_logger& test_logger){
+			inline void test_base_type(f_test_logger& test_logger){
 				test_logger.check("base_type",fsl::ver_1_0::is_same<   typename range_int<_BASE_TYPE,10,false,false>::base_type,   _BASE_TYPE   >::value);
 			}
 			inline void all(f_test_logger& test_logger){
 				test_logger.scoped("types",[&]{
-					base_type<uint8_t>(test_logger);
-					base_type<int16_t>(test_logger);
-					base_type<uint64_t>(test_logger);
-					base_type<int32_t>(test_logger);
+					test_base_type<uint8_t>(test_logger);
+					test_base_type<int16_t>(test_logger);
+					test_base_type<uint64_t>(test_logger);
+					test_base_type<int32_t>(test_logger);
 				});
 			}
 			using runner = typename task_runner::static_task_runner<all>;
 		} // namespace types
 		namespace public_member_functions {
-			
+			namespace constructors {
+				
+			}
 		}
 		using runner = concat<classes::runner, types::runner>;
 	}
